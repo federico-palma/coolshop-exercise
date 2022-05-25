4c4a8ced1ee33b17814d56bbbeb8d78a7ae80008
federico-palma <fedepal9@gmail.com>
2022-05-25 16:50:21 +0200

    Added live result on operator change

--- src/Components/RowComponent.js
+++ src/Components/RowComponent.js
@@ -1,15 +1,34 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import NumbersContext from "../Store-context/numbers-context";
 
 const RowComponent = (props) => {
   const numbersCtx = useContext(NumbersContext);
 
+  const [currentOperator, setCurrentOperator] = useState("+");
+  const [currentValue, setCurrentValue] = useState(0);
+
+  const handleChangeOperator = () => {
+    setCurrentOperator(currentOperator === "+" ? "-" : "+");
+  };
+
+  useEffect(() => {
+    numbersCtx.handleValueChange(
+      props.index,
+      parseInt(currentOperator + currentValue)
+    );
+    // eslint-disable-next-line
+  }, [currentOperator, currentValue]);
+
   return (
     <div className="row">
-      <button type="button" className="operator">
-        +/-
+      <button type="button" className="operator" onClick={handleChangeOperator}>
+        {currentOperator}
       </button>
-      <input type="number" placeholder="Enter a number" onChange={e => numbersCtx.handleValueChange(props.index, parseInt(e.target.value))}/>
+      <input
+        type="number"
+        placeholder="Enter a number"
+        onChange={(e) => setCurrentValue(e.target.value)}
+      />
       <button type="button" className="disable-btn">
         Disable
       </button>
