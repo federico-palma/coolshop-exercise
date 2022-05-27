71ab147f6ec16d3cf1465d653c0a5447eb282cc2
federico-palma <fedepal9@gmail.com>
2022-05-27 03:23:28 +0200

    Added title to tab

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import ResultComponent from "./Components/ResultComponent";
 import RowsContainerComponent from "./Components/RowsContainerComponent";
 import { NumbersContextProvider } from "./Store-context/numbers-context";
+import { useEffect } from "react"
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "React Challenge | Federico Palma"
+  }, [])
+
   return (
     <main className="App">
       <NumbersContextProvider>
