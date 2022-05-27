692b2fa6daee9180fdef4c98669b8648bb180acb
federico-palma <fedepal9@gmail.com>
2022-05-27 02:38:43 +0200

    Added conditional message on empty list

---
+++ src/Components/EmptyListMessageComponent.js
@@ -0,0 +1,10 @@
+const EmptyListMessageComponent = () => {
+  return (
+    <>
+      <li className="empty-list-message">↑</li>
+      <li className="empty-list-message">Click button to add new rows!</li>
+    </>
+  );
+};
+
+export default EmptyListMessageComponent;
--- src/Components/RowsContainerComponent.js
+++ src/Components/RowsContainerComponent.js
@@ -1,24 +1,33 @@
 import { useContext } from "react";
 import RowComponent from "./RowComponent";
+import EmptyListMessageComponent from "./EmptyListMessageComponent";
 import NumbersContext from "../Store-context/numbers-context";
 
 const RowsContainerComponent = () => {
   const numbersCtx = useContext(NumbersContext);
 
+  function Empty(params) {
+    
+  }
+
   return (
     <div className="row-container">
-      <button className="new-row-btn" onClick={numbersCtx.handleAddNewRow}>Add new row</button>
+      <button className="new-row-btn" onClick={numbersCtx.handleAddNewRow}>
+        Add new row
+      </button>
       <ul className="row-list">
-        {numbersCtx.rowsArray.map((element) => {
-          return (
-            <RowComponent
-              key={element.id}
-              id={element.id}
-              value={element.value}
-              enabled={element.enable}
-            ></RowComponent>
-          );
-        })}
+        {numbersCtx.rowsArray &&
+          numbersCtx.rowsArray.map((element) => {
+            return (
+              <RowComponent
+                key={element.id}
+                id={element.id}
+                value={element.value}
+                enabled={element.enable}
+              />
+            );
+          })}
+        {!numbersCtx.rowsArray.length && <EmptyListMessageComponent/>}
       </ul>
     </div>
   );
